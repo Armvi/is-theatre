2022e52a6c993724392bd26af3f4ddfcd1ef9631
Abyss <secretkeeper.ik@gmail.com>
2022-11-08 21:29:28 +0300

    Init logger, init config, call app.run()

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,18 @@
-package cmd
+package main
+
+import (
+	"is-theatre/config"
+	"is-theatre/internal/app"
+	"is-theatre/pkg/logger/logger_logrus"
+)
 
 func main() {
-	
+	log, err := logger_logrus.NewLogrusLogger()
+
+	cfg, err := config.NewAppConfig()
+	if err != nil {
+		(*log).Error("config init error:", err)
+	}
+
+	app.Run(cfg, log)
 }
